80ca11193f8bcf0efbd46d55704129a3d42c1c96
astudillome <projectagility@gmail.com>
2021-06-04 11:40:12 -0600

    solved node-sass module install error

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -1,13 +1,13 @@
 import json
-from flask import request, _request_ctx_stack, abort
+from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'mastudillo.us.auth0.com'
+AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'coffee_shop'
+API_AUDIENCE = 'dev'
 
 ## AuthError Exception
 '''
--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_filename = "coffee.db"
+database_filename = "database.db"
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
 
@@ -38,7 +38,9 @@ def db_drop_and_create_all():
         title='water',
         recipe='[{"name": "water", "color": "blue", "parts": 1}]'
     )
-    drink.insert()
+
+
+drink.insert()
 # ROUTES
 
 '''
